2f7ff2d78cadb41c87a11d4e9f53b215505785b1
Alastair Hendricks <hendricks.alastair@gmail.com>
2020-05-14 16:10:08 +0200

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Alastair Hendricks on 2020/05/14.
+//  Copyright © 2020 Alastair Hendricks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
